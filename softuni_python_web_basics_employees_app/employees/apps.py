306381df9e78d04978f795adf3790ea4f5d564fe
haraGADygyl <ti60gtt@gmail.com>
2022-01-26 18:02:02 +0200

    add skeleton

---
+++ softuni_python_web_basics_employees_app/employees/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmployeesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'softuni_python_web_basics_employees_app.employees'
