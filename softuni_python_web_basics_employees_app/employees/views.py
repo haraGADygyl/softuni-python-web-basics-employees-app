a659cce926ec2246cf1b9c13fd427c6ce72d673e
haraGADygyl <ti60gtt@gmail.com>
2022-01-27 13:07:34 +0200

    add urls

--- softuni_python_web_basics_employees_app/employees/views.py
+++ softuni_python_web_basics_employees_app/employees/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    return HttpResponse('This is home view')
+
+
+def department_details(request):
+    return HttpResponse('This is department 1 view')
+
+
+def list_departments(request):
+    return HttpResponse('This is list view')
